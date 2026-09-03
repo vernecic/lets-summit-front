cdc30829e6fe5d167eaa104a9ec7aa6750342a33
Vid Ernečić <vidernecic1@gmail.com>
2026-09-03 19:45:02 +0200

    delete trip

--- app/pages/trips/[id].vue
+++ app/pages/trips/[id].vue
@@ -12,12 +12,23 @@ const config = useRuntimeConfig();
 const route = useRoute();
 const params = route.params as { id: string };
 
+const deleteTrip = async () => {
+  await $fetch(`${config.public.BACKEND_URL}/trips/${params.id}`, {
+    method: "DELETE",
+    credentials: "include",
+  });
+  navigateTo("/");
+};
+
 const url = `${config.public.BACKEND_URL}/trips/${params.id}`;
-const { data: trip, error } = await useFetch<CardSummit>(url);
+const { data: trip } = await useFetch<CardSummit>(url);
 </script>
 
 <template>
-  <div v-if="trip" class="py-20 max-w-4xl mx-auto px-40 min-h-screen">
+  <div
+    v-if="trip"
+    class="py-20 flex flex-col gap-20 max-w-4xl mx-auto px-40 min-h-screen"
+  >
     <div class="flex gap-20">
       <div class="flex flex-col gap-6">
         <div class="flex gap-2 items-center">
@@ -31,6 +42,16 @@ const { data: trip, error } = await useFetch<CardSummit>(url);
         </div>
         <p>{{ trip.description }}</p>
       </div>
+    </div>
+    <div>
+      <UButton
+        @click="deleteTrip"
+        v-if="isUserOwner"
+        color="error"
+        variant="solid"
+        class="cursor-pointer"
+        >Delete Trip</UButton
+      >
     </div>
   </div>
 </template>
